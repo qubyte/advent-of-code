1ecafde39fcbded55817bf7b64a4f5c7047d2346
Mark S. Everitt <mark.s.everitt@gmail.com>
2021-12-12 00:50:01 +0000

    Adds solutions for 2021 day 11.

---
+++ 2021/11/task-02/src/main.rs
@@ -0,0 +1,74 @@
+use std::io::{stdin, BufRead};
+use std::collections::HashSet;
+
+fn main() {
+    let mut grid: Vec<Vec<usize>> = stdin()
+        .lock()
+        .lines()
+        .filter_map(|line_result| line_result.ok())
+        .map(|line| {
+            line.chars().map(|c| (c.to_string()).parse().unwrap()).collect()
+        })
+        .collect();
+
+    let max_row = grid.len() - 1;
+    let max_col = grid[0].len() - 1;
+    let octopuses = grid.len() * grid[0].len();
+    let mut step = 0usize;
+
+    loop {
+        step += 1;
+
+        for row in 0..grid.len() {
+            for col in 0..grid[row].len() {
+                grid[row][col] += 1;
+            }
+        }
+
+        let mut sparked: HashSet<(usize, usize)> = HashSet::new();
+
+        loop {
+            let mut count = 0usize;
+
+            for row in 0..grid.len() {
+                for col in 0..grid[row].len() {
+                    if grid[row][col] < 10 || sparked.contains(&(row, col)) {
+                        continue;
+                    }
+
+                    sparked.insert((row, col));
+                    count += 1;
+
+                    let from_row = row.checked_sub(1).unwrap_or(row);
+                    let from_col = col.checked_sub(1).unwrap_or(col);
+                    let to_row = max_row.min(row + 1);
+                    let to_col = max_col.min(col + 1);
+
+                    for r in from_row..=to_row {
+                        for c in from_col..=to_col {
+                            grid[r][c] += 1;
+                        }
+                    }
+                }
+            }
+
+            if count == 0  {
+                break;
+            }
+        }
+
+        if sparked.len() == octopuses {
+            break;
+        }
+
+        for row in 0..grid.len() {
+            for col in 0..grid[row].len() {
+                if grid[row][col] > 9 {
+                    grid[row][col] = 0;
+                }
+            }
+        }
+    }
+
+    println!("{}", step);
+}
