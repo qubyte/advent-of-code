8b4bc433a8265f3b50bed51f9bf1a47d28186c23
Mark S. Everitt <mark.s.everitt@gmail.com>
2021-12-16 20:30:12 +0000

    Adds solutions for 2021 day 15.

---
+++ 2021/15/task-01/src/main.rs
@@ -0,0 +1,99 @@
+use std::io::{stdin, BufRead};
+use std::collections::{HashMap, HashSet};
+
+fn main() {
+    let dangers: Vec<Vec<usize>> = stdin()
+        .lock()
+        .lines()
+        .filter_map(|line_result| line_result.ok())
+        .map(|line| {
+            line.chars().map(|c| c.to_string().parse().unwrap()).collect()
+        })
+        .collect();
+
+    let target = (dangers.len() - 1, dangers[dangers.len() - 1].len() - 1);
+
+    let mut vertices = HashSet::new();
+    let mut distances = HashMap::new();
+    let mut previous = HashMap::new();
+
+    for j in 0..dangers.len() {
+        for i in 0..dangers[j].len() {
+            distances.insert((i, j), usize::MAX);
+            vertices.insert((i, j));
+        }
+    }
+
+    distances.insert((0, 0), 0);
+
+    while vertices.len() != 0 {
+        let v_c = vertices.clone();
+        let vertex = v_c.iter().min_by(|a_coord, b_coord| {
+            let a = distances.get(a_coord).unwrap();
+            let b = distances.get(b_coord).unwrap();
+
+            a.cmp(&b)
+        }).unwrap();
+
+        vertices.remove(vertex);
+
+        let (i, j) = vertex;
+
+        if (*i, *j) == target {
+            break;
+        }
+
+        if *i > 0 {
+            let d = distances.get(&(*i, *j)).unwrap() + dangers[*j][*i - 1];
+
+            if d < *distances.get(&(*i - 1, *j)).unwrap() {
+                distances.insert((*i - 1, *j), d);
+                previous.insert((*i - 1, *j), (*i, *j));
+            }
+        }
+        if *i < dangers[*j].len() - 1 {
+            let d = distances.get(&(*i, *j)).unwrap() + dangers[*j][*i + 1];
+
+            if d < *distances.get(&(*i + 1, *j)).unwrap() {
+                distances.insert((*i + 1, *j), d);
+                previous.insert((*i + 1, *j), (*i, *j));
+            }
+        }
+        if *j > 0 {
+            let d = distances.get(&(*i, *j)).unwrap() + dangers[*j - 1][*i];
+
+            if d < *distances.get(&(*i, *j - 1)).unwrap() {
+                distances.insert((*i, *j - 1), d);
+                previous.insert((*i, *j - 1), (*i, *j));
+            }
+        }
+        if *j < dangers.len() - 1 {
+            let d = distances.get(&(*i, *j)).unwrap() + dangers[*j + 1][*i];
+
+            if d < *distances.get(&(*i, *j + 1)).unwrap() {
+                distances.insert((*i, *j + 1), d);
+                previous.insert((*i, *j + 1), (*i, *j));
+            }
+        }
+    }
+
+    let mut u = target;
+    let mut path = vec![u];
+
+    loop {
+        if let Some((i, j)) = previous.get(&u) {
+            u = (*i, *j);
+            path.push((*i, *j));
+        } else {
+            break;
+        }
+    }
+
+    let mut total_danger = 0usize;
+
+    for (i, j) in &path {
+        total_danger += dangers[*j][*i];
+    }
+
+    println!("{}", total_danger - dangers[0][0]);
+}
