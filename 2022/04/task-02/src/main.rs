503c14070f54af1b6a055b4be62d6ebd260482c5
Mark S. Everitt <mark.s.everitt@gmail.com>
2022-12-04 11:06:28 +0000

    Adds 2022 day 04.

---
+++ 2022/04/task-02/src/main.rs
@@ -0,0 +1,30 @@
+use std::io::{stdin, BufRead};
+
+fn parse_line(line: String) -> Option<((u32, u32), (u32, u32))> {
+    let split: Result<[_; 4], _> = line
+        .split(&['-', ','])
+        .map(|n| n.parse::<u32>())
+        .collect::<Vec<_>>()
+        .try_into();
+
+    match split {
+        Ok([Ok(a), Ok(b), Ok(c), Ok(d)]) => Some(((a, b), (c, d))),
+        _ => None
+    }
+}
+
+fn do_overlap(a: (u32, u32), b: (u32, u32)) -> bool {
+    a.0 <= b.1 && b.0 <= a.1
+}
+
+fn main() {
+    let count = stdin()
+        .lock()
+        .lines()
+        .filter_map(|line_result| line_result.ok())
+        .filter_map(parse_line)
+        .filter(|&(a, b)| do_overlap(a, b))
+        .count();
+
+    println!("{}", count);
+}
