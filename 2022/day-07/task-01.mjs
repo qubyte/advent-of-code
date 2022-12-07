c7be8fe9619954871e88902dc3f4ef53640824a5
Mark S. Everitt <mark.s.everitt@gmail.com>
2022-12-07 21:09:23 +0000

    Adds solutions for 2022 day 7.

---
+++ 2022/day-07/task-01.mjs
@@ -0,0 +1,49 @@
+import readline from 'node:readline';
+
+const rl = readline.createInterface({ input: process.stdin });
+const path = [];
+export const directories = { '/': { content: [], subdirectories: [] } };
+
+function buildPath(...elements) {
+    return elements.join('/').slice(1) || '/';
+}
+
+for await (const line of rl) {
+  if (line.startsWith("$ cd ")) {
+    let dir = line.slice(5).trim();
+
+    if (dir === '/') {
+        path.length = 0;
+        path.push(dir);
+    } else if (dir === '..') {
+        path.pop();
+    } else {
+        path.push(dir);
+        directories[buildPath(...path)] ||= { content: [], subdirectories: [] };
+    }
+  } else if (line === '$ ls') {
+    // Do nothing because the lines which follow are handled by branches below.
+  } else if (line.startsWith('dir')) {
+    directories[buildPath(...path)].subdirectories.push(buildPath(...path, line.slice(4)));
+  } else { // file
+    const [size, name] = line.split(' ');
+    directories[buildPath(...path)].content.push([parseInt(size, 10), name]);
+  }
+}
+
+const numDirectories = Object.keys(directories).length;
+const calculated = [];
+
+while (calculated.length < numDirectories) {
+    for (const [dir, info] of Object.entries(directories)) {
+        if (typeof info.size === 'undefined' && info.subdirectories.every(dir => calculated.includes(dir))) {
+            const contentSum = info.content.reduce((total, entry) => total + entry[0], 0);
+            const subdirSum = info.subdirectories.reduce((total, dir) => total + directories[dir].size, 0);
+            info.size = contentSum + subdirSum;
+            calculated.push(dir);
+        }
+    }
+}
+
+const sum = Object.entries(directories).reduce((total, [_, { size }]) => size <= 100000 ? total + size : total, 0);
+console.log(sum)
