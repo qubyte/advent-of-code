0e6dd64384b950c7a2ae43ac04d8913c3930a63d
Mark S. Everitt <mark.everitt@shopify.com>
2023-12-11 10:26:59 +0000

    Improvements to 2023 d11t2

--- 2023/day-11/task2.swift
+++ 2023/day-11/task2.swift
@@ -1,55 +1,58 @@
-struct Coord: Hashable {
-  let x: Int
+struct Galaxy: Hashable {
+  var x: Int
   let y: Int
 }
 
-var universe: [Coord] = []
+// The universe is an array of galaxy coordinates. No need to keep
+// track of the space between them.
+var universe: [Galaxy] = []
+var colsWithGalaxies: Set<Int> = Set()
 var row = 0
 var cols = 0
 
 let expansion = 1000000
 
 while let line = readLine() {
-  var count = 0
-
   cols = line.count
+
+  var hasGalaxies = false
 
   for (i, char) in line.enumerated() {
     if char == "#" {
-      count += 1
-      universe.append(Coord(x: i, y: row))
+      hasGalaxies = true
+      universe.append(Galaxy(x: i, y: row))
+      colsWithGalaxies.insert(i)
     }
   }
 
-  row += count == 0 ? expansion : 1
+  // Expansion can be handled for rows as the universe is read in
+  row += hasGalaxies ? 1 : expansion
 }
 
+// Columns have to be handled after the universe is injested.
 for x in (0..<cols).reversed() {
-  let galaxiesInColumn = universe.filter { $0.x == x }.count
-  var toRemove: [Coord] = []
-  var toAdd: [Coord] = []
-
-  if galaxiesInColumn == 0 {
-    for galaxy in universe {
-      if (galaxy.x > x) {
-        toAdd.append(Coord(x: galaxy.x + expansion - 1, y: galaxy.y))
-        toRemove.append(galaxy)
-      }
-    }
+  if colsWithGalaxies.contains(x) {
+    continue
   }
 
-  universe = universe.filter { !toRemove.contains($0) }
-  universe.append(contentsOf: toAdd)
+  // Galaxies of a higher column need to have the expansion added.
+  for i in 0..<universe.count {
+    if universe[i].x > x {
+      universe[i].x += expansion - 1
+    }
+  }
 }
 
 var total = 0
 
+// Pairs of galaxies, exactly once. Reminds me of my C days.
 for i in 0..<(universe.count - 1) {
   let galaxyA = universe[i]
 
   for j in (i + 1)..<universe.count {
     let galaxyB = universe[j]
 
+    // Manhatten distance. Surprised it took until day 11 to see it!
     total += abs(galaxyA.y - galaxyB.y) + abs(galaxyA.x - galaxyB.x)
   }
 }
